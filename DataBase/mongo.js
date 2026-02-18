b1fd1fbcf184039875ec728b31e82cff572dbb79
Rohit <rohitkumargiddi@gmail.com>
2026-02-18 17:49:39 +0530

    cloud database connection

--- DataBase/mongo.js
+++ DataBase/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://rohitkumargiddi:ryfwor-goznUz-0xutje@cluster0.fjlwgtf.mongodb.net/?appName=Cluster0"
+const url = process.env.DB
 mongoose.connect(url)
   .then(() => console.log('Database Connected!'))
   .catch((error)=>console.log("Mongoose connection fail",error));
